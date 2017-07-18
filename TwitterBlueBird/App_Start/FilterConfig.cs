986efa67eea1f1b0dfd5740744c331a817a61228
Wrenderer <cil.family@gmail.com>
2017-07-18 17:20:29 -0500

    Initial Commit

---
+++ TwitterBlueBird/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TwitterBlueBird
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ TwitterBlueBird/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwitterBlueBird.MvcApplication" Language="C#" %>
