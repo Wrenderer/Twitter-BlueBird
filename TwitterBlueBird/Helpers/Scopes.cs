f27df32c20731be36731fa476219ae16fc1cacbe
Wrenderer <cil.family@gmail.com>
2017-07-19 10:16:22 -0500

    Add Methods to grab "happy" or "angry" tweets

--- TwitterBlueBird/Helpers/Scopes.cs
+++ TwitterBlueBird/Helpers/Scopes.cs
@@ -7,17 +7,27 @@ namespace TwitterBlueBird.Helpers
 {
 	public static class Scope
 	{
-		public static List<Tweet> UnratedTweets(int limit = 5)
+		public static List<Tweet> UnratedTweets(int limit = 5, string mood = null)
 		{
 			using (var context = new TwitterAPIContainer())
 			{
 				return (from t in context.Tweets
-						where t.Mood == null
+						where t.Mood == mood
 						select t)
 						.OrderBy(x => Guid.NewGuid())
 						.Take(limit)
 						.ToList();
 			}
+		}
+
+		public static List<Tweet> HappyTweets(int limit = 1)
+		{
+			return UnratedTweets(limit, "Happy");
+		}
+
+		public static List<Tweet> AngryTweets(int limit = 1)
+		{
+			return UnratedTweets(limit, "Angry");
 		}
 	}
 }
