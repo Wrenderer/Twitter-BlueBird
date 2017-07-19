907966486ce7724d91ea0dae8f11fa3ffd49863f
Wrenderer <cil.family@gmail.com>
2017-07-19 14:56:56 -0500

    A User should be able to view the top 10 happy and angry words * Add ViewModel to pass in tweets and both happy and angry lists to index page * Add methods to scope class to determine happy or angry words

---
+++ TwitterBlueBird/ViewModels/IndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TwitterBlueBird.ViewModels
+{
+	public class IndexViewModel
+	{
+		public List<Tweet> RatableTweets { get; set; }
+		public List<Word> TopHappyWords { get; set; }
+		public List<Word> TopAngryWords { get; set; }
+
+		public IndexViewModel(List<Tweet> t, List<Word> h, List<Word> a)
+		{
+			RatableTweets = t;
+			TopHappyWords = h;
+			TopAngryWords = a;
+		}
+	}
+}
