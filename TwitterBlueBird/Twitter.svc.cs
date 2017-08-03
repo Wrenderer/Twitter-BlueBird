a10b21287c92ae5278bafb28266583a7c6ab718f
Wrenderer <cil.family@gmail.com>
2017-08-02 23:22:55 -0500

    Allow Users to Ask for a Single Random Tweet * Add WCF Tweet service for AJAX * Adjust margins on main page * Adjust layout of main page * Add single random tweet display * Add AJAX call button to display single random tweet on main page * Remove deprecated default tests

---
+++ TwitterBlueBird/Twitter.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using TwitterBlueBird.Helpers;
+
+namespace TwitterBlueBird
+{
+	[ServiceContract(Namespace = "TwitterService")]
+	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+	public class Twitter
+	{
+		[OperationContract]
+		[WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json)]
+		public List<String> GetTweets(int amount)
+		{
+			List<Tweet> random_tweets = Scope.UnratedTweets(amount);
+			return random_tweets.ConvertAll(x => x.Text);
+		}
+
+	}
+}
